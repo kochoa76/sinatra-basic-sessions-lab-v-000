80e74276ed154e99fafb2bfb7bdad4ea0919c621
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-17 18:04:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  configure do 
-    enable :sessions 
+  configure do
+    enable :sessions
     @sessions = session_secret, "secret"
 end
+end 
